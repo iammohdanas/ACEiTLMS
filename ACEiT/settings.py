d4a40629c9e4c412f0f3ff7db292a637aa562570
AATricks <aaanas372@gmail.com>
2024-01-27 18:28:15 +0530

    ready for railway deploy

--- ACEiT/settings.py
+++ ACEiT/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-54m05is&^@fopzs!-134v=dgh62h^r#koa&lm^88i)q$bjcyzd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,11 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
-STATICFILES_DIRS= [
-    os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS= [
+#     os.path.join(BASE_DIR,'static')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
